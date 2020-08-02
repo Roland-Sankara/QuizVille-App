fc45e397f1e0a5966e999fe79c47490213632fd7
Roland-Sankara <roland1sankara@gmail.com>
2020-08-03 02:17:29 +0300

    ft(add: integrate api)

---
+++ rank.js
@@ -0,0 +1,25 @@
+const highScoresList = document.querySelector('#highScoresList');
+//saving User record
+fetch('https://quizville-app.herokuapp.com/api/records/', {
+	method  : 'GET',
+	headers : { 'Content-Type': 'application/json', Authorization: `Bearer ${localStorage.getItem('token')}` }
+})
+	.then((res) => {
+		if (!res.ok) {
+			throw new Error('Something Went Wrong >> Failed to save records!');
+		} else {
+			return res.json();
+		}
+	})
+	.then((res) => {
+		let rankArray = res.results.sort((a, b) => b.score - a.score);
+		rankArray.splice(5);
+		highScoresList.innerHTML = rankArray
+			.map((result) => `<li class="high-score">${result.username} => ${result.score}</li>`)
+			.join('');
+		alert('good');
+	})
+	.catch((err) => {
+		alert(err);
+		console.log(err);
+	});
